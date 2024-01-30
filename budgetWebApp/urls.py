8159a83689ec1de9732843b24e3b4d8b1bce8ab4
ingli0 <inbogia@cs.ihu.gr>
2024-01-30 15:53:57 +0200

    added urls

--- budgetWebApp/urls.py
+++ budgetWebApp/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('',include('managebudget.urls')),
+    path('authentication/',include('authentication.urls')),
     path('admin/', admin.site.urls)
 ]
