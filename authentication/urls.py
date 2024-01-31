39c55fb4d36cac3cbce6d6f6734d4c58632d70cc
ingli0 <inbogia@cs.ihu.gr>
2024-01-31 17:29:22 +0200

    added email validation

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,11 @@
-from .views import RegistrationView,UsernameValdationView
+from .views import RegistrationView,UsernameValdationView, EmailValdationView
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('register',RegistrationView.as_view(),name="register"),
     path('validate-username', csrf_exempt(UsernameValdationView.as_view()),
-                                        name="validate-username")
+                                        name="validate-username"),
+    path('validate-email',csrf_exempt( EmailValdationView.as_view()), name='validate-email')
+                                    
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -5,17 +5,29 @@ from django.http import JsonResponse
 import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
+from  validate_email import validate_email 
 # Create your views here.
 
+class EmailValdationView(View):
+    def post(self,request):
+        data = json.loads(request.body)
+        email = data['email']
+        if not validate_email(email):
+            return JsonResponse({'email_error':'Email is invalid'},status=400)
+        
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error':'Email is already in use'},status=409)
+        return JsonResponse({'email_valid':True})
+        
 class UsernameValdationView(View):
     def post(self,request):
         data = json.loads(request.body)
         username = data['username']
         if not str(username).isalnum():
-            return JsonResponse({'username error':'usearname should only contain alphanueric characters'},status=400)
+            return JsonResponse({'username_error':'usearname should only contain alphanueric characters'},status=400)
         
         if User.objects.filter(username=username).exists():
-            return JsonResponse({'username error':'usearname is taken'},status=409)
+            return JsonResponse({'username_error':'usearname is taken'},status=409)
         return JsonResponse({'username_valid':True})
         
 class RegistrationView(View):
