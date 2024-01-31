f7aabf6e9bb80efc9632411e0c5730a44713a798
ingli0 <inbogia@cs.ihu.gr>
2024-01-31 19:21:24 +0200

    bugs fix

--- budgetWebApp/static/js/register.js
+++ budgetWebApp/static/js/register.js
@@ -1,9 +1,24 @@
 const usernameField = document.querySelector("#usernameField");
 const feedBackArea = document.querySelector(".invalid_feedback"); 
 const emailField = document.querySelector("#emailField");
+const passwordField = document.querySelector("#passwordField"); // Corrected selector
 const emailFeedBackArea = document.querySelector(".emailFeedBackArea");
+const showPasswordBtn = document.querySelector(".showpassWordToggle");
 const usernameSuccessOutput = document.querySelector('.usernameSuccessOutput');
+const showpassWordToggle = document.querySelector(".showpassWordToggle");
 
+const handleToggleInput = (e) => {
+    if (showPasswordBtn.textContent === 'Show') {
+        showPasswordBtn.textContent = 'Hide';  
+        passwordField.setAttribute('type', "text");
+
+    } else {
+        showPasswordBtn.textContent = 'Show';  
+        passwordField.setAttribute('type', "password");
+    }
+};
+
+showpassWordToggle.addEventListener('click', handleToggleInput);
 
 
 emailField.addEventListener('keyup',(e)=>{
