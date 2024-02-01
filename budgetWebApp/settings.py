c034b60597e188250de1dec691a030d60bb33f4c
ingli0 <inbogia@cs.ihu.gr>
2024-02-01 15:30:17 +0200

    register data

--- budgetWebApp/settings.py
+++ budgetWebApp/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from django.contrib import messages
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,8 +137,7 @@ MESSAGE_TAGS={
     messages.ERROR:'danger'
 }
 
-ef
-feefef
+ 
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_USE_TLS = True
