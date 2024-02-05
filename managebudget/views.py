f56790b22bddba664ec99bc8636abc82f5f67dad
ingli0 <inbogia@cs.ihu.gr>
2024-02-05 20:12:13 +0200

    Added messages

--- managebudget/views.py
+++ managebudget/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Category,Expense
 # Create your views here.
+from django.contrib import messages 
 
 
 @login_required(login_url='/authentication/login')
@@ -10,8 +11,20 @@ def index(request):
     return render(request,'managebudget/index.html')
 
 def add_managebudget(request):
-    categories=Category.objects.all()
+    categories = Category.objects.all()
     context={
-        'categories':categories
-    }
-    return render(request,'managebudget/add_managebudget.html',context)
+            'categories':categories
+        }
+    if request.method == 'GET':
+        
+        return render(request,'managebudget/add_managebudget.html',context)
+
+    if request.method =='POST':
+        amount=request.POST['amount']
+
+        if not amount :
+            messages.error(request,'Amount is required')
+            return render(request, 'managebudget/add_managebudget.html', context)
+
+        
+     
