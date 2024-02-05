21478698035e6c9fcbcb5b8aee4c03ab11a66ffc
ingli0 <inbogia@cs.ihu.gr>
2024-02-05 19:18:43 +0200

    Added dynamic select options

--- managebudget/views.py
+++ managebudget/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Category,Expense
 # Create your views here.
 
 
 @login_required(login_url='/authentication/login')
 def index(request):
+    categories=Category.objects.all()
     return render(request,'managebudget/index.html')
 
 def add_managebudget(request):
-    return render(request,'managebudget/add_managebudget.html')
+    categories=Category.objects.all()
+    context={
+        'categories':categories
+    }
+    return render(request,'managebudget/add_managebudget.html',context)
