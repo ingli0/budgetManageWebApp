2e31a7c18d69e373a6527f11aed7848102630252
ingli0 <inbogia@cs.ihu.gr>
2024-02-07 20:39:51 +0200

    Added ajax  search

--- managebudget/urls.py
+++ managebudget/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
-
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('', views.index, name="managebudget"),
     path('add-managebudget',views.add_managebudget, name="add-managebudget"),
     path('edit-expense/<int:id>',views.expense_edit, name="edit-expense"),
-    path('expense-delete/<int:id>',views.delete_expense, name="expense-delete")
+    path('expense-delete/<int:id>',views.delete_expense, name="expense-delete"),
+    path('search-expenses',csrf_exempt(views.search_expenses), name="search_expenses")
 ]
+ 
