d2e016fdde834f664c114474a2730323102441a8
ingli0 <inbogia@cs.ihu.gr>
2024-02-13 19:02:55 +0200

    added style in admin page

--- userincome/admin.py
+++ userincome/admin.py
@@ -3,5 +3,12 @@ from .models import UserIncome,Source
 # Register your models here.
 
 
-admin.site.register(UserIncome)
+class ExpensesAdmin(admin.ModelAdmin):
+    list_display = ('amount','date','description','owner','source')
+    search_fields = ('amount','date','description','owner','source')
+    list_per_page = 5
+    
+
+admin.site.register(UserIncome,ExpensesAdmin)
 admin.site.register(Source)
+
